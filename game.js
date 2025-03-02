57ea7a347dc181a52fef6d9c2fe8e1ab7766f6f0
Ross Coombs <2410466@dundee.ac.uk>
2025-03-02 17:23:23 +0000

    added calculate distance

--- game.js
+++ game.js
@@ -2,10 +2,23 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        document.getElementById("currentMessage").innerHTML = "Geolocatio not supported :(";
     }
+    document.getElementById("currentMessage").innerHTML = calcDistance(56.4559872, -2.998272, 56.457922, -2.981056);
 }
 
 function showPosition(position) {
     document.getElementById("currentMessage").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+//calculate distance using the haversine formula
+function calcDistance(lat1, lon1, lat2, lon2) {
+    //convert distances in lat and long into radians
+    const toRad = (angle) => angle * (Math.PI / 180);
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
+    //haversine formula
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return 6371 * c; // Distance in km (6371 is the radius of Earth in km)
 }
